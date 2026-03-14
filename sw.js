837f4818e302d514043148457d0fd7e15cca5044
DrStang <48525932+DrStang@users.noreply.github.com>
2026-03-14 15:04:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,6 @@
-const CACHE_NAME = 'coffee-tycoon-v3';
-const ASSETS = ['/', '/index.html', '/style.css', '/game.js', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
+const CACHE = 'ct-v4';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
-  // Network-first for HTML/JS/CSS to ensure updates propagate
-  if (e.request.url.match(/\.(html|js|css)(\?|$)/)) {
-    e.respondWith(
-      fetch(e.request).then(r => {
-        const clone = r.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-        return r;
-      }).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
-  }
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
